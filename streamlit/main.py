9fad140ced9b41a89feec226e6b0da7cfe43595f
Tony Sheller <asheller@gmail.com>
2023-11-20 17:53:42 +0000

    Sarted on streamlit interface

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,7 +1,19 @@
 import streamlit as st
+from kubernetes import client, config 
+
+def set_config():
+    '''
+    Return it from one spot so only 
+    need to change it in one spot.
+    '''
+    config.load_incluster_config()
+    #config.load_kube_config()
+
+
+
 
 # Sidebar
-st.sidebar.title("Sidebar Title")
+st.sidebar.title("Configuration")
 
 # Add widgets to the sidebar
 user_input = st.sidebar.text_input("Enter something:")
@@ -9,7 +21,19 @@ selected_option = st.sidebar.selectbox("Select an option:", ["Option 1", "Option
 button_clicked = st.sidebar.button("Click Me")
 
 # Main Content
-st.title("Main Content")
+st.title("DRL Agent as a Kubernetes Scheduler")
+st.subheader('EN.705.742 Advanced Applied Machine Learning')
+
+st.markdown(
+"""
+        **Overview**
+    
+        Information about GPU. this uses the Python Moduel 
+        nvsmi 
+     
+        """)
+
+
 
 # Display user input and selected option in the main content area
 st.write(f"You entered: {user_input}")

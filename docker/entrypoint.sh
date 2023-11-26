e8f6e7616caba9241c9936bbb0e7e917a7561cce
Tony Sheller <asheller@gmail.com>
2023-11-26 10:50:26 -0500

    adjusted entrypoint to use scripts folder

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 
 # Run the Python script
-python kwok_cluster/create_nodes.py --node_count=10
+python scripts/create_nodes.py --node_count=10
 
 # Use supervisord to start the srevices we need.
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
